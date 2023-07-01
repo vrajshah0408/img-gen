725560ba538686b5ba3b73236c9bbf65d6812a52
Vraj Shah <vrajshah8@gmail.com>
2023-06-30 20:54:27 -0400

    image open api works working on custom image

---
+++ server.js
@@ -0,0 +1,57 @@
+/** @format */
+
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+const fs = require('fs')
+const multer = require('multer')
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) =>{
+        console.log('file')
+        cb(null, Date.now()+ "-"+ file.originalname)
+    }
+})
+
+const upload = multer({storage: storage}).single('file')
+
+//Routes
+app.post("/images", async (req, res) => {
+  try{
+    const response = await openai.createImage({
+        prompt: req.body.message,
+        n: 2,
+        size: "1024x1024",
+      })
+      console.log(response.data.data)
+      res.send(response.data.data)
+  }catch(error){
+    console.error(error)
+  }
+});
+
+app.post("/upload", (req, res) => {
+ upload(req, res, (err) =>{
+   if (err instanceof multer.MulterError){
+    return res.status(500).json(err)
+   }else if (err){
+    return res.status(500).json(err)
+   }
+   console.log(req.file)
+ })
+})
+
+app.listen(PORT, () => console.log("Server Running on PORT:" + PORT));
--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import { response } from "express";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState(null);
+  const [value, setValue] = useState(null);
+  const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage]= useState(null);
+  const supriseOption = [
+    "A red car driving backwards",
+    "A blue bike riding on water",
+    "A greem plane flying backwards",
+    "A yellow boat swimming on land",
+  ];
+
+  const uploadImage = async(e) =>{
+    console.log(e.target.files[0])
+
+    const formData = new FormData()
+    formData.append('file', e.target.files[0])
+    setSelectedImage(e.target.files[0])
+
+    try{
+      const options ={
+        method: "POST",
+        body: formData
+      }
+      await fetch('http://localhost:8000/upload',options)
+      const data =await response.json()
+      
+    }catch(error){
+      console.error(error)
+    }
+  }
+  const supriseMe = () => {
+    setImages(null);
+    const randomValue =
+      supriseOption[Math.floor(Math.random() * supriseOption.length)];
+    setValue(randomValue);
+  };
+
+  const getImages = async () => {
+    setImages(null);
+    if (value === null) {
+      setError("Error Must have search term!");
+      return;
+    }
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({ message: value }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/images", options);
+      const data = await response.json();
+      console.log(data);
+      setImages(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <section className="search-section">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start with a detailed description
+          <span className="suprise" onClick={supriseMe}>
+            Suprise Me
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="input-container">
+          <input
+            value={value}
+            placeholder="An impressionist oil painting of a sunflower in a red vase"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button onClick={getImages}>Generate</button>
+        </div>
+        <p className="extra-info">
+          Or,
+          <span>
+            <label htmlFor="files"> Upload an image </label>
+            <input onChange={uploadImage} id="files" accept="image/*" type="file" hidden />
+          </span>
+          to edit
+        </p>
+        {error && <p>{error}</p>}
+      </section>
+      <section className="image-section">
+        {images?.map((image, _index) => (
+          <img
+            key={_index}
+            src={image.url}
+            alt={`Generated image of ${value}`}
+          />
+        ))}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
